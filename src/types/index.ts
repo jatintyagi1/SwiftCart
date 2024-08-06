d34d86dde4381b9107eebb8af939bdfecd768fe7
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-08-06 13:15:22 +0530

    made sepaarate file for types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,15 @@
 
+
+// Customer Signup Types
 export interface SignUpFormData{
     email: String;
     mobileNumber: String;
     password: String;
     confirmPassword: String;
+}
+
+// customer Login Types  ( import in customer.service..ts file )
+export interface LoginFormData {
+    identifier: string; // This can be either email or mobileNumber
+    password: string;
 }
