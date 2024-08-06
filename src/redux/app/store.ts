1bb8c351b5e4de55959b279316aeea120372dfc0
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-08-06 13:18:32 +0530

    add login reducer

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,17 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../feautures/auth/authSlice';
+import loginReducer from '../feautures/auth/loginSlice'
 
 
 export const store = configureStore({
     reducer: {
       auth: authReducer,
+      login: loginReducer,
     },
   });
   
   export type RootState = ReturnType<typeof store.getState>;
   export type AppDispatch = typeof store.dispatch;
-
-
-
-
 
