f0b28cadd0d0581f14410aace503cf344af6ccef
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-09-13 11:36:07 +0530

    create a Not Found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Result } from 'antd';
+// import { useNavigate } from 'react-router-dom'; // Assuming you're using react-router-dom
+
+const NotFoundPage = () => {
+    //const navigate = useNavigate();
+
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={
+                <Button type="primary"
+                    //onClick={() => navigate('/')}
+                >
+                    Back to Home
+                </Button>
+            }
+        />
+    );
+};
+
+export default NotFoundPage;
